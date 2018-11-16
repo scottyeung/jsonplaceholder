85be0d2df0a6f67e1a1269af6a72f7db4dc8cc59
Scott Yeung <micold@gmail.com>
2018-11-16 23:42:31 +0000

    added model.[] to computed()

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -2,14 +2,21 @@ import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 
 export default Controller.extend({
+    init(){
+        this._super(...arguments);
+
+        this.model = this.get('model');
+    },
     filter: '',
-    filteredItems: computed('filter', function(){
+    filteredItems: computed('model.[]', 'filter', function(){
         const filterTerm = this.get('filter');
         var model = this.get('model');
 
         var filtered = model.filter(function(post) {
             return post.get('username').indexOf(filterTerm) !== -1;
-        });
+        }
+        
+        );
         return filtered
     })
 });
