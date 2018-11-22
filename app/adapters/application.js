2aec07a67062323e27bb9222ca6b6941192f7a52
scottyeung <micold@gmail.com>
2018-11-22 11:42:33 +0000

    Update application.js

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,12 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-    host: 'https://jsonplaceholder.typicode.com',
-    primaryKey: '_id',
-    ajaxOptions() {
-        let hash = this._super.apply(this, arguments);
-        hash.data = hash.data || {};
-        hash.dataType = 'jsonp';
-        return hash;
-    }
+    host: 'https://jsonplaceholder.typicode.com'
 });
