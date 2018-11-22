1c553fced2a401128b0688ee0b17c8a5fc7f9418
scottyeung <micold@gmail.com>
2018-11-22 11:44:21 +0000

    Update users.js

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -12,7 +12,8 @@ export default Controller.extend({
         const filterTerm = this.get('filter').toLowerCase();
 
         let filtered = this.model.filter((post)=>{
-            return post.get('username').toLowerCase().indexOf(filterTerm) !== -1;
+            const {username, email} = post.getProperties('username','email');
+            return post.get('username').toLowerCase().indexOf(filterTerm) !== -1 ? true : post.get('email').toLowerCase().indexOf(filterTerm) !== -1;
         });
         
         return filtered;
